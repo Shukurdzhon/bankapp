b779b387fa91d08bf260c6f59fc6f4769ad8590c
Shukur <shukur_007_96@mail.ru>
2020-09-17 09:25:43 +0500

    добавлены типы

---
+++ pkg/bank/types/types.go
@@ -0,0 +1,35 @@
+package types
+
+// Money represents the amount of money in (diram, cents, kopeyka end etc.) 
+type Money int64
+
+//Currency represents the code of currency
+type Currency string
+
+//codes of currencies
+const (
+	TJS Currency = "TJS"
+	RUB Currency = "RUB"
+	USD Currency = "USD"
+)
+
+//PAN represents the card number
+type PAN string
+
+//Card rpresents information about card
+type Card struct {
+	ID         int
+	PAN        PAN
+	Balance    Money
+	Currency   Currency
+	Color      string
+	Name       string
+	Active     bool
+	MinBalance Money
+}
+
+//Payment represents information about payment
+type Payment struct {
+	ID int
+	Amount Money
+}
